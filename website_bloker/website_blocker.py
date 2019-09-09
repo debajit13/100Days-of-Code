5d64787f35ed547ab70a68bebfe902aaad12a267
debajit13 <mallickdebajit3@gmail.com>
2019-09-09 15:35:15 +0530

    update website_blocker.py

--- website_bloker/website_blocker.py
+++ website_bloker/website_blocker.py
@@ -1,14 +1,30 @@
 import time
 from datetime import datetime as dt
 
+hosts_temp = r"D:\Python Mega Course\website_bloker\hosts"
 hosts_path = r"C:\Windows\System32\drivers\etc"
 redirect = "127.0.0.1"
 website_list = ["www.facebook.com","facebook.com"]
 
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
-        print("Working Hour")
+        print("Working Hour...")
+        with open(hosts_path, 'r+') as  file:
+            content = file.read()
+            for website in website_list:
+                if website in  content:
+                    pass
+                else:
+                    file.write(redirect+" "+website+"\n")
+
 
     else:
-        print("Fun Hour")
+        with open(hosts_path,'r+') as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("Fun hours...")
     time.sleep(5)
