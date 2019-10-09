a09508d3187ef389dc4e7362296dde459f6068a1
debajit13 <mallickdebajit3@gmail.com>
2019-10-09 23:34:30 +0530

    String Palindrom check added!

---
+++ checkStringPalindromOrNot.py
@@ -0,0 +1,17 @@
+def reverse(s):
+	return s[::-1]
+
+def isPalindrome(s):
+	rev = reverse(s)
+
+	if (s == rev):
+		return True
+	return False
+
+s = "bob"
+ans = isPalindrome(s)
+
+if ans == 1:
+	print("Yes")
+else:
+	print("No")
