c442cdf9d37b3c20d975867e8b4d8677d8bee399
debajit13 <mallickdebajit3@gmail.com>
2019-11-04 00:02:56 +0530

    update checkpalindrom

--- checkPalindromOrnot.py
+++ checkPalindromOrnot.py
@@ -1,19 +1,17 @@
-# function which return reverse of a string
 def reverse(s):
 	return s[::-1]
 
 def isPalindrome(s):
-	# Calling reverse function
+
 	rev = reverse(s)
 
-	# Checking if both string are equal or not
 	if (s == rev):
 		return True
 	return False
 
 
-# Driver code
-s = "malayalam"
+
+s = "mam"
 ans = isPalindrome(s)
 
 if ans == 1:
