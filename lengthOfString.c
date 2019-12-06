8c2edb310c29c22a3e4349ba26a919d92cfd67db
debajit13 <mallickdebajit3@gmail.com>
2019-12-06 23:42:01 +0530

    string length c code file added!

---
+++ lengthOfString.c
@@ -0,0 +1,13 @@
+//program to calculate the length of the string
+#include <stdio.h>
+int main()
+{
+    char s[1000];
+    int i;
+    printf("Enter a string: ");
+    scanf("%s", s);
+    for(i = 0; s[i] != '\0'; ++i);
+    printf("Length of string: %d", i);
+    return 0;
+}
+
